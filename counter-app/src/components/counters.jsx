aa59ecf993b868f1f8268fe4b48b18264d9f32b4
Daniel Tien <dtien@Daniels-MacBook-Pro.local>
2020-08-02 21:08:29 -0700

    lifting state up

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -11,16 +11,43 @@ class Counters extends Component {
     ],
   };
 
-  render() {
-    console.log(this.props);
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
 
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+    // console.log("Event Handler Called", counterId);
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
           ></Counter>
         ))}
       </div>
